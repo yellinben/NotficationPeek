630ba3d00761808669ce2e77355523a2357fe989
Ben Yellin <YellinBen@gmail.com>
2011-08-19 20:23:26 -0400

    Replaced company name

--- NotificationPeek/NotificationPeek.h
+++ NotificationPeek/NotificationPeek.h
@@ -3,7 +3,7 @@
 //  NotificationPeek
 //
 //  Created by Ben Yellin on 8/19/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 Been Yelling. All rights reserved.
 //
 
 
